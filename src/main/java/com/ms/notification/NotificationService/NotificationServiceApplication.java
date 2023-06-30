5e448902bdc1e3f9f00a95d4d5fd44a12e7595fa
Aayushi <aayushi@Aayushis-MacBook-Air.local>
2023-06-30 12:00:25 +0530

    notificationServiceCommit

--- src/main/java/com/ms/notification/NotificationService/NotificationServiceApplication.java
+++ src/main/java/com/ms/notification/NotificationService/NotificationServiceApplication.java
@@ -1,9 +1,14 @@
 package com.ms.notification.NotificationService;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
